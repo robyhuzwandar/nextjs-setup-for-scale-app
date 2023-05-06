3b6710a66df830531aa7e56de91dfe1de4bbb07f
Roby Huzwandar <roby.huzwandar@flash-coffee.com>
2023-05-06 16:41:49 +0800

    project initialization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
